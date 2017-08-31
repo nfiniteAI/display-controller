0369ddc8910310ee43566a74f4c73553605cee92
Matt Fisher <mattf@vimeo.com>
2017-08-31 11:35:23 -0400

    Update dist/ to v2.1.1

--- dist/player.js
+++ dist/player.js
@@ -1,4 +1,4 @@
-/*! @vimeo/player v2.1.0 | (c) 2017 Vimeo | MIT License | https://github.com/vimeo/player.js */
+/*! @vimeo/player v2.1.1 | (c) 2017 Vimeo | MIT License | https://github.com/vimeo/player.js */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
