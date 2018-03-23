f23792f39c4f5ac9a922a2b751bfe3a046caf5a0
Wesley Luyten <hi@wesleyluyten.com>
2018-03-23 11:21:09 -0400

    v2.4.0

--- dist/player.js
+++ dist/player.js
@@ -1,4 +1,4 @@
-/*! @vimeo/player v2.3.1 | (c) 2018 Vimeo | MIT License | https://github.com/vimeo/player.js */
+/*! @vimeo/player v2.4.0 | (c) 2018 Vimeo | MIT License | https://github.com/vimeo/player.js */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
@@ -815,7 +815,7 @@ function swapCallbacks(oldElement, newElement) {
  * @module lib/embed
  */
 
-var oEmbedParameters = ['id', 'url', 'width', 'maxwidth', 'height', 'maxheight', 'portrait', 'title', 'byline', 'color', 'playsinline', 'autoplay', 'autopause', 'loop', 'responsive', 'speed', 'background', 'transparent'];
+var oEmbedParameters = ['autopause', 'autoplay', 'background', 'byline', 'color', 'height', 'id', 'loop', 'maxheight', 'maxwidth', 'muted', 'playsinline', 'portrait', 'responsive', 'speed', 'title', 'transparent', 'url', 'width'];
 
 /**
  * Get the 'data-vimeo'-prefixed attributes from an element as an object.
