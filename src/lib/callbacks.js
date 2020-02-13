c04e163ec47f6fc6ebe82ed265fd434ba72a82d3
Guillaume Hertault <gh@hubstairs.com>
2020-02-13 19:33:59 +0100

    feat: player to display in callbacks

--- src/lib/callbacks.js
+++ src/lib/callbacks.js
@@ -5,86 +5,86 @@
 export const callbackMap = new WeakMap()
 
 /**
- * Store a callback for a method or event for a player.
+ * Store a callback for a method or event for a display.
  *
- * @param {Player} player The player object.
+ * @param {Display} display The display object.
  * @param {string} name The method or event name.
- * @param {(function(this:Player, *): void|{resolve: function, reject: function})} callback
+ * @param {(function(this:Display, *): void|{resolve: function, reject: function})} callback
  *        The callback to call or an object with resolve and reject functions for a promise.
  * @return {void}
  */
-export function storeCallback(player, name, callback) {
-  const playerCallbacks = callbackMap.get(player.element) || {}
+export function storeCallback(display, name, callback) {
+  const displayCallbacks = callbackMap.get(display.element) || {}
 
-  if (!(name in playerCallbacks)) {
-    playerCallbacks[name] = []
+  if (!(name in displayCallbacks)) {
+    displayCallbacks[name] = []
   }
 
-  playerCallbacks[name].push(callback)
-  callbackMap.set(player.element, playerCallbacks)
+  displayCallbacks[name].push(callback)
+  callbackMap.set(display.element, displayCallbacks)
 }
 
 /**
- * Get the callbacks for a player and event or method.
+ * Get the callbacks for a display and event or method.
  *
- * @param {Player} player The player object.
+ * @param {Display} display The display object.
  * @param {string} name The method or event name
  * @return {function[]}
  */
-export function getCallbacks(player, name) {
-  const playerCallbacks = callbackMap.get(player.element) || {}
-  return playerCallbacks[name] || []
+export function getCallbacks(display, name) {
+  const displayCallbacks = callbackMap.get(display.element) || {}
+  return displayCallbacks[name] || []
 }
 
 /**
- * Remove a stored callback for a method or event for a player.
+ * Remove a stored callback for a method or event for a display.
  *
- * @param {Player} player The player object.
+ * @param {Display} display The display object.
  * @param {string} name The method or event name
  * @param {function} [callback] The specific callback to remove.
  * @return {boolean} Was this the last callback?
  */
-export function removeCallback(player, name, callback) {
-  const playerCallbacks = callbackMap.get(player.element) || {}
+export function removeCallback(display, name, callback) {
+  const displayCallbacks = callbackMap.get(display.element) || {}
 
-  if (!playerCallbacks[name]) {
+  if (!displayCallbacks[name]) {
     return true
   }
 
   // If no callback is passed, remove all callbacks for the event
   if (!callback) {
-    playerCallbacks[name] = []
-    callbackMap.set(player.element, playerCallbacks)
+    displayCallbacks[name] = []
+    callbackMap.set(display.element, displayCallbacks)
 
     return true
   }
 
-  const index = playerCallbacks[name].indexOf(callback)
+  const index = displayCallbacks[name].indexOf(callback)
 
   if (index !== -1) {
-    playerCallbacks[name].splice(index, 1)
+    displayCallbacks[name].splice(index, 1)
   }
 
-  callbackMap.set(player.element, playerCallbacks)
-  return playerCallbacks[name] && playerCallbacks[name].length === 0
+  callbackMap.set(display.element, displayCallbacks)
+  return displayCallbacks[name] && displayCallbacks[name].length === 0
 }
 
 /**
- * Return the first stored callback for a player and event or method.
+ * Return the first stored callback for a display and event or method.
  *
- * @param {Player} player The player object.
+ * @param {Display} display The display object.
  * @param {string} name The method or event name.
  * @return {function} The callback, or false if there were none
  */
-export function shiftCallbacks(player, name) {
-  const playerCallbacks = getCallbacks(player, name)
+export function shiftCallbacks(display, name) {
+  const displayCallbacks = getCallbacks(display, name)
 
-  if (playerCallbacks.length < 1) {
+  if (displayCallbacks.length < 1) {
     return false
   }
 
-  const callback = playerCallbacks.shift()
-  removeCallback(player, name, callback)
+  const callback = displayCallbacks.shift()
+  removeCallback(display, name, callback)
   return callback
 }
 
@@ -96,8 +96,8 @@ export function shiftCallbacks(player, name) {
  * @return {void}
  */
 export function swapCallbacks(oldElement, newElement) {
-  const playerCallbacks = callbackMap.get(oldElement)
+  const displayCallbacks = callbackMap.get(oldElement)
 
-  callbackMap.set(newElement, playerCallbacks)
+  callbackMap.set(newElement, displayCallbacks)
   callbackMap.delete(oldElement)
 }
