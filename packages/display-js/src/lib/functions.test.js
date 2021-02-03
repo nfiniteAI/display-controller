2522d1cb3e8975c3a121f1280863d25901b7293e
Guillaume Hertault <gh@hubstairs.com>
2021-02-03 20:49:08 +0100

    test: add missing tests

--- packages/display-js/src/lib/functions.test.js
+++ packages/display-js/src/lib/functions.test.js
@@ -53,6 +53,7 @@ describe('isObjectId', () => {
 
 describe('isHubstairsUrl', () => {
   test('identifies right Hubstairs urls only', () => {
+    expect(isHubstairsUrl('http://display.nfinite.app')).toBe(true)
     expect(isHubstairsUrl('http://display.hubstairs.com')).toBe(true)
     expect(isHubstairsUrl('https://display.hubstairs.com')).toBe(true)
     expect(isHubstairsUrl('//display.hubstairs.com')).toBe(true)
