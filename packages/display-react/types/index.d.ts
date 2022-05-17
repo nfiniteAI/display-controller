9c09cb1e7ebf93af5ae7e358510f6a6939c107f9
Guillaume Hertault <gh@hubstairs.com>
2022-05-17 16:47:31 -0400

    fix: types

--- packages/display-react/types/index.d.ts
+++ packages/display-react/types/index.d.ts
@@ -6,43 +6,43 @@ type onProductClick = (product: Product) => void;
 // TODO
 type onFilter = (filter: Filter) => void;
 type onChangeScene = (scene: { products: Product[] }) => void;
-type onChangeProduct = (productChange: {prevProduct: Product, nextProduct: Product }) => void;
+type onChangeProduct = (productChange: { prevProduct: Product; nextProduct: Product }) => void;
 
 export enum Language {
-  'fr-FR' = 'fr-FR',
-  'en-US' ='en-US'
+  FR_FR = 'fr-FR',
+  EN_US = 'en-US',
 }
 
 type Product = {
-  id:  String,
-  code: String,
-  nfiniteCode: String
-}
+  id: string;
+  code: string;
+  nfiniteCode: string;
+};
 
-type Filter = any
+type Filter = any;
 
 type DisplayBaseProps = {
-  token: String,
-  productcode?: String,
-  config?: any,
-  oembedUrl?: String,
-  responsive?: Boolean,
-  onError?: onError,
-  onProductClick?: onProductClick,
-  onReady?: onReady,
-  onFilter?: onFilter,
-  onChangeScene?: onChangeScene,
-  onChangeProduct?: onChangeProduct,
-  language?: Language,
-}
+  token: string;
+  productcode?: string;
+  config?: any;
+  oembedUrl?: string;
+  responsive?: boolean;
+  onError?: onError;
+  onProductClick?: onProductClick;
+  onReady?: onReady;
+  onFilter?: onFilter;
+  onChangeScene?: onChangeScene;
+  onChangeProduct?: onChangeProduct;
+  language?: Language;
+};
 
 export type DisplayIdProps = {
-  displayid: String;
-} & DisplayBaseProps
+  displayid: string;
+} & DisplayBaseProps;
 
 export type DisplayUrlProps = {
-  displayurl: String;
-} & DisplayBaseProps
+  displayurl: string;
+} & DisplayBaseProps;
 
 export default function Display(props: DisplayIdProps);
 export default function Display(props: DisplayUrlProps);
