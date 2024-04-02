6e979cacfb1e6aed16940f26384271266ffec46e
Romain Julien <romainj.contact@gmail.com>
2024-04-02 10:56:08 +0200

    feat: selector dynamic-display-model-island according to displayType

--- packages/display-js/src/lib/embed.js
+++ packages/display-js/src/lib/embed.js
@@ -47,13 +47,15 @@ function generateSelectorFromElement(webComponentElement) {
  * @param {HTMLElement} element The element to put the iframe in.
  * @return {HTMLIFrameElement} The iframe embed.
  */
-export function createEmbedJS({ html }, element) {
+export function createEmbedJS({ displayType, html }, element) {
   if (!element) {
     throw new HubstairsError('An element must be provided', 'TypeError')
   }
 
+  const webComponentSelector = displayType === 'MODEL_AR' ? 'dynamic-display-model-island' : 'dynamic-display-island'
+
   if (element.getAttribute('data-hubstairs-initialized') !== null) {
-    return element.querySelector('dynamic-display-island')
+    return element.querySelector(webComponentSelector)
   }
 
   const div = document.createElement('div')
@@ -88,7 +90,7 @@ export function createEmbedJS({ html }, element) {
   element.appendChild(div.firstChild)
   element.setAttribute('data-hubstairs-initialized', 'true')
 
-  const webComponentElement = element.querySelector('dynamic-display-island')
+  const webComponentElement = element.querySelector(webComponentSelector)
 
   if (window.__NfiniteDisplay && typeof window.__NfiniteDisplay.render === 'function') {
     window.__NfiniteDisplay.render({
